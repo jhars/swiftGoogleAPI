90515d2937ea5940680ec909178072fd08024909
jhars <jjharlan06@gmail.com>
2015-10-26 20:24:55 -0700

    Initial Commit

---
+++ GoogleNewsAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleNewsAPI
+//
+//  Created by Jon Harlan on 10/26/15.
+//  Copyright © 2015 Jon Harlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
